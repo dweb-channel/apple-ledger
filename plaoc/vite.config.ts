701a424a0b281cb00ffc40b402c41d04d97148d5
waterbang <water_bang@163.com>
2024-10-01 15:48:13 +0800

    :boom: Change vue3

--- plaoc/vite.config.ts
+++ plaoc/vite.config.ts
@@ -1,6 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
